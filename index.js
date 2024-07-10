8bdec2e696bb1b6018f7408ba5f0b59cf5e48e50
yogeshsunder <sunderyogesh@gmail.com>
2024-07-10 19:09:57 +0530

    Making connection with mysql and getting data

---
+++ index.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
+});
+con.connect((err)=>{
+    if(err)
+    {
+        console.warn("error")
+    }
+    else{
+        console.warn("connected")
+    }
+})
+
+con.query("select * from persons",(err, result)=>{
+    console.warn("result", result)
+
+})
